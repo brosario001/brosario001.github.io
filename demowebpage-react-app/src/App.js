a5048726fe6ddf0c939c74c457195b9842d3a308
brosario001 <73300384+brosario001@users.noreply.github.com>
2025-07-24 15:26:32 -0400

    Launch of DemoPage001
    This demo page is an attempt to recreate Tyler the Creator's webpage for Don't Tap the Glass. I want to attempt to do this using React.JS, and React-Spring.js in an attempt to practice using these technologies.
    
    This is the first push, with about 3 hours of work.

---
+++ demowebpage-react-app/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import SlimShady from './img/Eminem.png';
+
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import { useSpring, animated } from '@react-spring/web';
+
+function Eminem(){
+  return(
+      <img 
+        src={SlimShady}
+        alt=""
+        className="eminem"
+      />
+  )
+}
+
+function App() {
+  return (
+    <div class = "App">
+      <Eminem/>
+
+      <Parallax pages = {3}>
+
+        <ParallaxLayer offset={.2} speed={.05} >
+          <h1 className = "AlbumName">Real SlimShady</h1>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0.05} factor={1.5}>
+          <h2>TEXT LAYER 1</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={2} speed={0.05} factor={3}>
+          <h2>TEXT LAYER 2</h2>
+        </ParallaxLayer>
+      </Parallax>
+      
+    </div>
+  );
+}
+export default App;
